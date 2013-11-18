db095a9561837a4dbd623eb2a69da93defca0653
thegeneral <jackjackie1212@gmail.com>
2013-11-18 11:18:22 -0800

    added drag

--- Final Project.rkt
+++ Final Project.rkt
@@ -1,3 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname |Final Project|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
 (require rsound)
 (require 2htdp/universe)
 (require 2htdp/image)
@@ -215,7 +218,7 @@
 ; mouse handler handles mouse events.
 ; Depending on where the user clicked, toggles a square.
 (define (me-h LOS x y event)
-  (cond [(equal? event "button-down")
+  (cond [(or (equal? event "button-down") (equal? event "drag"))
          (both (play (mapRowtoSound (y-pt->y-gd y)))
                (cond
                  [(or (negative? (y-pt->y-gd y))
